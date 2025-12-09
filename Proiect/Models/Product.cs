68acbaf55d4181d36a4c9c5c2f15328f93fe15ad
ModernityRejecter <andreidanielciocoiu@gmail.com>
2025-12-09 20:05:40 +0200

    Added early Category model implementation, added additional Review model constraints

---
+++ Proiect/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proiect.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product name can't be null")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Product description can't be null")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Product price ")]
+        public double Price { get; set; }
+
+
+    }
+}
