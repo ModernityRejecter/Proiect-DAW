222c9d7ccab3037104c46bbb88b24e54f5859838
ModernityRejecter <andreidanielciocoiu@gmail.com>
2025-12-09 19:55:29 +0200

    Added correct early Review model implementation

--- Proiect/Models/Review.cs
+++ Proiect/Models/Review.cs
@@ -8,8 +8,19 @@ namespace Proiect.Models
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Content { get; set; }
-        public double Rating { get; set; }
-        public int UserId { get; set; }
+        
+        [Range(1, 5)]
+        public int? Rating { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        //-----------------------------------------------------
+        public string UserId { get; set; }
         public virtual ApplicationUser User { get; set; }
+
+        //-----------------------------------------------------
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        //-----------------------------------------------------
     }
 }
