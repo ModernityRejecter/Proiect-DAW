65ee0f067e221bc5a5fc7994c2a225d9a2c95818
ModernityRejecter <andreidanielciocoiu@gmail.com>
2025-12-09 21:35:56 +0200

    Added early ProductFAQs model implementation

---
+++ Proiect/Models/ProductFAQs.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proiect.Models
+{
+    public class ProductFAQs
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Question field is required")]
+        public string Question { get; set; }
+
+        [Required(ErrorMessage = "Answer field is required")]
+        public string Answer { get; set; }
+
+        //------------------------------------------------------
+
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        //------------------------------------------------------
+    }
+}
