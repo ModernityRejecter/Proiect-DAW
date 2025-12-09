a491e2fbac6935d52dbbc295b45db19e1bfd7d37
ModernityRejecter <andreidanielciocoiu@gmail.com>
2025-12-09 21:31:02 +0200

    Added early ProposalFeedback model implementation

---
+++ Proiect/Models/ProposalFeedback.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Proiect.Models
+{
+    public class ProposalFeedback
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage =  "Message is required")]
+        [StringLength(1000, ErrorMessage = "Message must be at most 1000 characters long")]
+        public string Message { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        //--------------------------------------------------------
+        
+        public int UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set;}
+
+        //--------------------------------------------------------
+        
+        public int ProposalId { get; set; }
+
+        public virtual ProductProposal Proposal { get; set; }
+
+        //--------------------------------------------------------
+    }
+}
