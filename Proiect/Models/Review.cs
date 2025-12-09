2d37a2262f48e56fcd1f18e0ba06658477cffd63
ModernityRejecter <andreidanielciocoiu@gmail.com>
2025-12-09 19:52:36 +0200

    Added early Review model implementation

---
+++ Proiect/Models/Review.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proiect.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public double Rating { get; set; }
+        public int UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
