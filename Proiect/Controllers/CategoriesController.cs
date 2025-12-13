c5198097632e60ed70c0cd0d3b9cac4eadc10fc5
Oreo80 <Oreo80@users.noreply.github.com>
2025-12-13 22:53:40 +0200

    Categories controller CRUD+ views  + actualizat navbar

---
+++ Proiect/Controllers/CategoriesController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proiect.Data;
+using Proiect.Models;
+using System.Linq;
+
+
+namespace Proiect.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CategoriesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager) : Controller
+    {
+        private readonly ApplicationDbContext db = context;
+        private readonly UserManager<ApplicationUser> _userManager = userManager;
+        private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+
+        public ActionResult Index()
+        {
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.message = TempData["message"]?.ToString();
+            }
+
+            var categories = from category in db.Categories
+                             orderby category.Name
+                             select category;
+            ViewBag.Categories = categories;
+            return View();
+        }
+
+        public ActionResult Show(int id)
+        {
+            Category? category = db.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(cat);
+                db.SaveChanges();
+                TempData["message"] = "Categoria a fost adaugata";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(cat);
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Category? category = db.Categories.Find(id);
+
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Category requestCategory)
+        {
+            Category? category = db.Categories.Find(id);
+
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                category.Name = requestCategory.Name;
+                db.SaveChanges();
+                TempData["message"] = "Categoria a fost modificata!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(requestCategory);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            Category? category = db.Categories.Find(id);
+
+            if (category is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                db.Categories.Remove(category);
+                TempData["message"] = "Categoria a fost stearsa";
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
