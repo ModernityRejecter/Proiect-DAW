4946293ed970afee0b8d66a1c9e027b526946f8b
ModernityRejecter <andreidanielciocoiu@gmail.com>
2025-12-27 20:25:14 +0200

    Made preparations for feedback implementation

---
+++ Proiect/Controllers/ProposalFeedbacksController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Proiect.Data;
+using Proiect.Models;
+
+namespace Proiect.Controllers
+{
+    public class ProposalFeedbacksController(ApplicationDbContext context, UserManager<ApplicationUser>
+        userManager, RoleManager<IdentityRole> roleManager) : Controller
+    {
+        private readonly ApplicationDbContext db = context;
+        private readonly UserManager<ApplicationUser> _userManager = userManager;
+        private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+
+        [Authorize(Roles = "Admin,Colaborator")]
+
+        public IActionResult Index()
+        {
+            ICollection<ProposalFeedback> texts = new List<ProposalFeedback>();
+            texts = db.ProposalFeedbacks.ToList();
+            return View(texts);
+        }
+
+
+        //------------------------------------------------------------------------
+        // metode interne
+        //private ICollection<ProposalFeedback> GetAllTexts()
+        //{
+
+
+        //}
+
+    }
+}
